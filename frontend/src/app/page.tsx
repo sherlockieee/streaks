0569abc77abbafa88590fd98bfd76ed2054492b5
sherlockieee <phuonghatrannguyen00@gmail.com>
2023-07-13 17:05:40 -0700

    create prisma

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,10 +5,10 @@ import happySunIcon from "../../public/icons/happy-sun.svg";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="w-full max-w-lg bg-primary p-10  border-black border-solid border-4 rounded-2xl flex flex-col gap-8">
+      <div className="w-full max-w-md bg-primary p-10  border-black border-solid border-4 rounded-2xl flex flex-col gap-8">
         <div className="flex flex-row justify-between items-center">
           <h2 className="underline text-xl">Your streaks</h2>
-          <button className="border-2 border-black border-solid rounded-md font-serif uppercase font-bold bg-white px-4 py-2 ">
+          <button className="border-4 border-black border-solid rounded-md font-serif uppercase font-bold bg-white px-4 py-2 hover:bg-gray-100">
             Share
           </button>
         </div>
@@ -37,8 +37,7 @@ export default function Home() {
         <div>
           <h2 className="underline text-xl">Friends</h2>
           <div>
-            <div></div>
-            <a>Ha</a>
+            <p>Coming soon...</p>
           </div>
         </div>
       </div>
