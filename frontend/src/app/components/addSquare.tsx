8f152f35fa47d7a820bc0ecfab14856d924d4b96
sherlockieee <phuonghatrannguyen00@gmail.com>
2023-07-15 10:21:57 -0700

    add bouncing animation

--- frontend/src/app/components/addSquare.tsx
+++ frontend/src/app/components/addSquare.tsx
@@ -3,7 +3,7 @@ export function AddSquare() {
     <div
       className={`flex rounded-2xl border-black border-4 flex-col justify-center items-center p-4 
           bg-white
-      gap-2 aspect-square h-40 text-5xl font-bold`}
+      gap-2 aspect-square h-40 text-5xl font-bold cursor-pointer hover:bg-gray-100 bouncing-transition`}
     >
       {" "}
       +{" "}
