4fb5d86f5f57202410920b8d41312137132402a1
sherlockieee <phuonghatrannguyen00@gmail.com>
2023-07-17 00:08:04 -0700

    create checkbox reusable component

--- frontend/app/add/page.tsx
+++ frontend/app/add/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
+import Checkbox from "@/components/checkbox";
 import { useState, useEffect } from "react";
-import * as Checkbox from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
 
 export default function Add() {
   const [title, setTitle] = useState("");
@@ -10,10 +9,7 @@ export default function Add() {
   const [evidenceRequired, setEvidenceRequired] = useState(true);
   const [streakFreeze, setStreakFreeze] = useState(true);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log({ title, minimumTime, evidenceRequired, streakFreeze });
-  }
+  function handleSubmit() {}
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
@@ -31,17 +27,8 @@ export default function Add() {
         ></input>
         <div className="mb-2">
           <p className="font-bold">I want to do this:</p>
-          <div className="flex items-center ">
-            <Checkbox.Root
-              className="appearance-none bg-white rounded-sm flex items-center content-center mr-2 w-4 h-4"
-              defaultChecked
-              required
-              id="every-day"
-            >
-              <Checkbox.Indicator className="text-black">
-                <CheckIcon />
-              </Checkbox.Indicator>
-            </Checkbox.Root>
+          <div className="flex items-center">
+            <Checkbox defaultChecked required id="every-day" />
             <label htmlFor="every-day">Every day</label>
           </div>
         </div>
@@ -59,34 +46,25 @@ export default function Add() {
           ></input>
           <label htmlFor="minutes">minutes</label>
         </div>
-        <div className="flex items-center ">
-          <Checkbox.Root
-            className="appearance-none bg-white rounded-sm flex items-center content-center mr-2 w-4 h-4"
+        <div className="flex items-center">
+          <Checkbox
             defaultChecked
             value={evidenceRequired}
-            id="evidence"
+            id="require-evidence"
             onClick={() => setEvidenceRequired(!evidenceRequired)}
-          >
-            <Checkbox.Indicator className="text-black">
-              <CheckIcon />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
-          <label htmlFor="evidence">Require uploading evidence</label>
+          />
+          <label htmlFor="require-evidence">Require uploading evidence</label>
         </div>
-        <div className="flex items-center mb-8 ">
-          <Checkbox.Root
-            className="appearance-none bg-white rounded-sm flex items-center content-center mr-2 w-4 h-4"
+        <div className="flex items-center mb-4">
+          <Checkbox
             defaultChecked
-            id="streak-freeze"
             value={streakFreeze}
+            id="streak-freeze"
             onClick={() => setStreakFreeze(!streakFreeze)}
-          >
-            <Checkbox.Indicator className="text-black">
-              <CheckIcon />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <label htmlFor="streak-freeze">Allow streak freezes</label>
         </div>
+
         <button
           type="submit"
           className="border-4 border-black border-solid rounded-md font-serif uppercase font-bold bg-white px-4 py-2 hover:bg-gray-100"
--- frontend/components/checkbox.tsx
+++ frontend/components/checkbox.tsx
@@ -1,21 +1,22 @@
 "use client";
+
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
 
-export default function Checkbox() {
+interface CheckboxProps extends RadixCheckbox.CheckboxProps {
+  id: string;
+}
+
+export default function Checkbox({ id, ...rest }: CheckboxProps) {
   return (
-    <div className="flex items-center ">
-      <RadixCheckbox.Root
-        className="appearance-none bg-white rounded-sm flex items-center content-center mr-2 w-4 h-4"
-        defaultChecked
-        required
-        id="every-day"
-      >
-        <RadixCheckbox.Indicator className="text-black">
-          <CheckIcon />
-        </RadixCheckbox.Indicator>
-      </RadixCheckbox.Root>
-      <label htmlFor="every-day">Every day</label>
-    </div>
+    <RadixCheckbox.Root
+      className="appearance-none bg-white rounded-sm flex items-center content-center mr-2 w-4 h-4"
+      id={id}
+      {...rest}
+    >
+      <RadixCheckbox.Indicator className="text-black">
+        <CheckIcon />
+      </RadixCheckbox.Indicator>
+    </RadixCheckbox.Root>
   );
 }
